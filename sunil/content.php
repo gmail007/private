1545610f73cb6615f2c43b9fdbbe11610fe977cb
gmail007 <ankitdubeymail1@gmail.com>
2018-03-26 14:35:28 +0530

    initial commit

---
+++ sunil/content.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Content page</title>
+    <link rel="stylesheet" href="cssFiles/bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="cssFiles/gmailcontent.css">
+</head>
+<body>
+<h1> Content Page(Gmail) </h1>
+<p> &nbsp</p>
+<div class="container">
+    <div class="card-body">
+        <table class="table">
+            <tbody>
+            <tr>
+                <td>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox">
+                            <span class="form-check-sign"></span>
+                        </label>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                </td>
+
+                <td class="text-left">      </td>
+                <td class="td-actions text-right">
+                    Time
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox" checked="">
+                            <span class="form-check-sign"></span>
+                        </label>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                </td>
+
+                <td class="text-left">             </td>
+                <td class="td-actions text-right">
+                    Time
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox">
+                            <span class="form-check-sign"></span>
+                        </label>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                </td>
+
+                <td class="text-left">
+                </td>
+                <td class="td-actions text-right">
+                    Time
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="form-check">
+                        <label class="form-check-label">
+                            <input class="form-check-input" type="checkbox">
+                            <span class="form-check-sign"></span>
+                        </label>
+                        <i class="fa fa-star-o"></i>
+                    </div>
+                </td>
+
+                <td class="text-left">
+                </td>
+                <td class="td-actions text-right">
+                    Time
+                </td>
+            </tr>
+
+            </tbody>
+        </table>
+    </div>
+    <div class="footer">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="left">
+                    <p> 3.23 GB (21%) of 15 GB used </p>
+                    <p style="margin-left: 40px;"> <u>Manage</u> </p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="mid">
+                    <p><center>Terms - Privacy</center></p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="right">
+                    <p> Last account activity: minutes ago</p>
+                    <p> <u>Details</u> </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ sunil/sidebar.php
@@ -0,0 +1,34 @@
+<!DOCTYPEhtml>
+<html lang="en">
+<head>
+    <title>sunil</title>
+    <meta charset="utf-8">
+    <meta name="view point" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="side_bar.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="head">
+            <h1>Gmail<i class="fa fa-sort-desc" aria-hidden="true"></i></h1>
+
+        </div>
+        <div class="compose">
+            <p><b>COMPOSE<b></p>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="#">Inbox(925)</a></li>
+                <li><a href="#">Starred</a></li>
+                <li><a href="#">Important</a></li>
+                <li><a href="#">Sent Mail</a></li>
+                <li><a href="#">Drafts(37)</a></li>
+                <li><a href="#">Categories<i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+</body>
+</html>
